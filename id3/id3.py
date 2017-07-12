249adb2e40bde5a30291fb9ed9bb23a44697c983
Michael Noronha <michaeltnoronha@gmail.com>
2017-07-12 16:17:31 -0400

    Changed data format for easier partitioning

--- id3/id3.py
+++ id3/id3.py
@@ -1,62 +1,83 @@
 import math
 
-data = {
-        "play": ["no", "no", "yes", "yes", "yes", "no", "yes", "no", "yes", "yes", "yes", "yes", "yes", "no"],
-        "windy": ["false", "true", "false", "false", "false", "true", "true", "false", "false", "false", "true", "true", "false", "true"],
-        "outlook": ["sunny", "sunny", "overcast", "rainy", "rainy", "rainy", "overcast", "sunny", "sunny", "rainy", "sunny", "overcast", "overcast", "rainy"],
-        "temperature": ["hot", "hot", "hot", "mild", "cool", "cool", "cool", "mild", "cool", "mild", "mild", "mild", "hot", "mild"],
-        "humidity": ["high", "high", "high", "high", "normal", "normal", "normal", "high", "normal", "normal", "normal", "high", "normal", "high"]
-}
+OUTPUT_ATTR = "play"
+data = [
+         {"humidity": "high", "outlook": "sunny", "play": "no", "temperature": "hot", "windy": "false"},
+         {"humidity": "high", "outlook": "sunny", "play": "no", "temperature": "hot", "windy": "true"},
+         {"humidity": "high", "outlook": "overcast", "play": "yes", "temperature": "hot", "windy": "false"},
+         {"humidity": "high", "outlook": "rainy", "play": "yes", "temperature": "mild", "windy": "false"},
+         {"humidity": "normal", "outlook": "rainy", "play": "yes", "temperature": "cool", "windy": "false"},
+         {"humidity": "normal", "outlook": "rainy", "play": "no", "temperature": "cool", "windy": "true"},
+         {"humidity": "normal", "outlook": "overcast", "play": "yes", "temperature": "cool", "windy": "true"},
+         {"humidity": "high", "outlook": "sunny", "play": "no", "temperature": "mild", "windy": "false"},
+         {"humidity": "normal", "outlook": "sunny", "play": "yes", "temperature": "cool", "windy": "false"},
+         {"humidity": "normal", "outlook": "rainy", "play": "yes", "temperature": "mild", "windy": "false"},
+         {"humidity": "normal", "outlook": "sunny", "play": "yes", "temperature": "mild", "windy": "true"},
+         {"humidity": "high", "outlook": "overcast", "play": "yes", "temperature": "mild", "windy": "true"},
+         {"humidity": "normal", "outlook": "overcast", "play": "yes", "temperature": "hot", "windy": "false"},
+         {"humidity": "high", "outlook": "rainy", "play": "no", "temperature": "mild", "windy": "true"}
+]
 
-length = len(data["play"])
+# length = len(data["play"])
 
 class node:
-    def __init__(self,col,val,results,tNodes,fNodes):
-        self.col = col
-        self.val = val
-        self.results = results
-        self.tNodes = tNodes
-        self.fNodes = fNodes
+    def __init__(self,col,val,arc):
+        self.attr = attr          # attribute that was chosen to split
+        self.val = val            # at the leafs of the tree, will be a bucket
+        self.arc = arc
+
+class arc:
+    def __init__(self,attrVal,node):
+        self.attrVal = attrVal
+        self.node = node
+
+class tree:
+    def __init__(self,root):
+        self.root = root
 
 def calculateEntropy(data,outAttr):
-    a = {}
-    for val in data[outAttr]:
-        if a.has_key(val):
-            a[val] += 1
+    perType = {}
+    for record in data:
+        if perType.has_key(record[outAttr]):
+            perType[record[outAttr]] += 1
         else:
-            a[val] = 1.0
+            perType[record[outAttr]] = 1.0
 
     ret = 0
-    for x in a:
-        freq = a[x] / length
+    for x in perType:
+        freq = perType[x] / len(data)
         ret -= freq * math.log(freq,2)
 
     return ret
 
 def calculateGain(data,inAttr,outAttr):
-    a = {}
-    for val in data[inAttr]:
-        if a.has_key(val):
-            a[val] += 1
+    perType = {}
+    for record in data:
+        if perType.has_key(record[inAttr]):
+            perType[record[inAttr]] += 1
         else:
-            a[val] = 1.0
+            perType[record[inAttr]] = 1.0
 
-    b = zip(data[outAttr],data[inAttr])
-    c = {}
-    for pair in b:
-        if c.has_key(pair):
-            c[pair] += 1
+    conditionalPairs = {}
+    for record in data:
+        key = (record[inAttr],record[outAttr])
+        if conditionalPairs.has_key(key):
+            conditionalPairs[key] += 1
         else:
-            c[pair] = 1.0
+            conditionalPairs[key] = 1.0
 
     ret = 0
-    for x in c:
-        ret -= a[x[1]] / 14 * c[x] / a[x[1]] * math.log(c[x] / a[x[1]],2)
+    for x in conditionalPairs:
+        ret -= perType[x[0]] / 14 * conditionalPairs[x] / perType[x[0]] * math.log(conditionalPairs[x] / perType[x[0]],2)
 
     return calculateEntropy(data,"play") - ret
 
+# def divideOn(data,attr,value):
+
 def id3():
-    print calculateGain(data,"temperature","play")
+    # print calculateEntropy(data,OUTPUT_ATTR)
+    print calculateGain(data,"temperature",OUTPUT_ATTR)
+    # divideOn(data,"temperature","cool")
 
 id3()
 
